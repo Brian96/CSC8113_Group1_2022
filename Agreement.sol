e3e95308cc3a7a176abddc24850d929ff77d4ab2
Aayushi Gupta <aayushigupta@Aayushis-MacBook-Pro.local>
2022-03-16 10:43:19 +0000

    AggrementCont

--- Agreement.sol
+++ Agreement.sol
@@ -8,4 +8,12 @@ contract Agreement {
         uint userId;
         uint consent;
     }
+    mapping(uint => agreementInfo) agreements;
+
+    function setAgreement(agreementInfo memory agreement) public {
+        agreements[agreement.userId] = agreement;
+    }
+    function getAgreement(uint userId) public view returns (agreementInfo memory agreement){
+        return agreements[userId];
+    }
 }
