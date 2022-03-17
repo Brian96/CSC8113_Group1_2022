95a7d9018c7c3c6a17c85263d038cdab7127d559
aswinkv <a.kambian-veettil2@newcastle.ac.uk>
2022-03-17 19:23:02 +0000

    actor data collection purpose submission event

--- DataUsage.sol
+++ DataUsage.sol
@@ -9,13 +9,24 @@ contract DataUsage {
         string operation;
         string[] data;
     }
+
+    event DataCollection(
+        address indexed from,
+        actorInfo info
+    );
+
     mapping(uint => actorInfo) actors;
 
-    function setActor(actorInfo memory actor) public {
+    function setActor(actorInfo memory actor) private {
         actors[actor.actorId] = actor;
     }
 
     function getActor(uint actorId) public view returns (actorInfo memory actor) {
         return actors[actorId];
+    }
+
+    function submitPurpose(actorInfo memory actor) external {
+        actors[actor.actorId] = actor;
+        emit DataCollection(msg.sender, actors[actor.actorId]);
     }
 }
