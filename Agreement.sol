26b984a60eb6d0e7e40c16811848d61470789351
aswinkv <a.kambian-veettil2@newcastle.ac.uk>
2022-03-18 18:38:41 +0000

    user consent event

--- Agreement.sol
+++ Agreement.sol
@@ -4,16 +4,24 @@ pragma solidity 0.8.12;
 contract Agreement {
     mapping(uint => bool) public userConsent;
     struct agreementInfo{
-        uint256 hashAddr;
         uint userId;
         uint consent;
     }
+    event DataConsent(
+        address indexed from,
+        agreementInfo userConsent
+    );
     mapping(uint => agreementInfo) agreements;
 
-    function setAgreement(agreementInfo memory agreement) public {
+    function setAgreement(agreementInfo memory agreement) private {
         agreements[agreement.userId] = agreement;
     }
     function getAgreement(uint userId) public view returns (agreementInfo memory agreement){
         return agreements[userId];
+    }
+
+    function submitConsent(agreementInfo memory user) external {
+        agreements[user.userId] = user;
+        emit DataConsent(msg.sender, agreements[user.userId]);
     }
 }
