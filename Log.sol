2633de4cce6260706b139d0ceed89eeb17c53423
Aayushi Gupta <aayushigupta@newhuaweiapstudent-10-59-8-111.ncl.ac.uk>
2022-03-20 17:38:56 +0000

    log.sol

--- Log.sol
+++ Log.sol
@@ -2,10 +2,25 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Log {
-    struct LogInfo{
+    struct logInfo{
         uint actorId;
         string operation;
-        string[] data;
+        string[3] data;
         string serviceName;
+    }
+    mapping(uint => logInfo) logs;
+    function setLog(uint actorId, string memory operation, string[3] memory data, string memory serviceName) public{
+        logInfo memory log;
+        log.actorId = actorId;
+        log.operation = operation;
+        log.data = data;
+        log.serviceName = serviceName;
+        logs[actorId] = log;
+    }
+    function getLog(uint actorId) public view returns (logInfo memory log){
+        return logs[actorId];
+    }
+    function updateLog(uint actorId, string memory operation) public {
+        logs[actorId].operation = string(abi.encodePacked(logs[actorId].operation, operation));
     }
 }
