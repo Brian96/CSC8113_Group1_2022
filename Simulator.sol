05dec363d05e0cc845ce03e979d26089bbb3ed1c
Licheng Wei <lichengwei@LichengdeMacBook-Pro.local>
2022-03-21 12:07:48 +0000

    add test case actor = 2

--- Simulator.sol
+++ Simulator.sol
@@ -5,24 +5,42 @@ import "./DataUsage.sol";
 import "./Agreement.sol";
 
 contract Simulator {
-    DataUsage actor;
+    DataUsage actors;
     Agreement dataSubject;
     uint u0;
     uint u1;
     constructor() {
         u0 = gasleft();
-        actor = new DataUsage();
-        actor.setActor(1, "test", "advertisement", "", ["Billy Bob", "01-01-1960", "07878780002", "Newcastle upon Tyne"]);
+        //actorCount1();
+        actorCount2();
+        //actorCount3();
+        //actorCount4();
+        //actorCount5();
+        u1 = gasleft();
+    }
+
+    function actorCount1() private{
+        actors = new DataUsage();
+        actors.setActor(1, "test", "advertisement", "", ["Billy Bob", "01-01-1960", "07878780002", "Newcastle upon Tyne"]);
         dataSubject = new Agreement();
         dataSubject.setAgreement(2, true);
         bool consent = dataSubject.getAgreement(2);
         if(consent) {
-            actor.updateActorOperation(1, "read");
-            actor.updateActorOperation(2, "share");
+            actors.updateActorOperation(1, "read");
         }
+    }
 
-
-        u1 = gasleft();
+    function actorCount2() private{
+        actors = new DataUsage();
+        actors.setActor(1, "test1", "advertisement1", "", ["Billy Bob", "01-01-1960", "07878780002", "Newcastle upon Tyne"]);
+        actors.setActor(2, "test2", "advertisement2", "", ["Anne", "01-01-1970", "07878780001", "London"]);
+        dataSubject = new Agreement();
+        dataSubject.setAgreement(2, true);
+        bool consent = dataSubject.getAgreement(2);
+        if(consent) {
+            actors.updateActorOperation(1, "read");
+            actors.updateActorOperation(2, "share");
+        }
     }
 
     function getGasCost() public view returns (uint gas){
