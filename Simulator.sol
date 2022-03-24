b91d2476b1c385e889fd9e64eb60ebf10339752d
Licheng Wei <lichengwei@newhuaweiapstudent-10-58-202-152.ncl.ac.uk>
2022-03-24 14:03:46 +0000

    fix

--- Simulator.sol
+++ Simulator.sol
@@ -16,7 +16,7 @@ contract Simulator {
     uint u0;
     uint u1;
 
-    mapping(uint => address) actorAddresses;
+    mapping(uint => address) actorAddresses;    //actorId (1-10) to address
     mapping(uint => address) userAddresses;
     mapping(uint => address) logAddresses;
     mapping(uint => address) verificationAddress;
@@ -26,49 +26,47 @@ contract Simulator {
         actorAddresses[1] = 0xd9145CCE52D386f254917e481eB44e9943F39138;
         userAddresses[1] = 0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8;
         logAddresses[1] = 0xf8e81D47203A594245E36C48e151709F0C19fBe8;
-        verificationAddress[1] = 0xDA0bab807633f07f013f94DD0E6A4F96F8742B53;
+        verificationAddress[1] = 0xD7ACd2a9FD159E69Bb102A1ca21C9a3e3A5F771B;
 
+        actorAddresses[2] = 0x7EF2e0048f5bAeDe046f6BF797943daF4ED8CB47;
+        actorAddresses[3] = 0xDA0bab807633f07f013f94DD0E6A4F96F8742B53;
 
         u0 = gasleft();
-        preset(1);
-        logAndValidation(1);
+        
+        preset(2);
+        logAndValidation(2);
 
-        //actorCount2();
-        //actorCount3();
-        //actorCount4();
-        //actorCount5();
         u1 = gasleft();
     }
 
 
     //set the purposed and data required of actors with number of maxActor
     function preset(uint maxActor) private{
-        if (maxActor == 1){
-            for (uint i = 1; i <= maxActor; i++){   //preset values to actors (DataUsage Contract)
-                actors.push(DataUsage(actorAddresses[1]));
-                actors[i-1].setActor(i, "test", "advertisement", "read", ["name", "address", "1", "3"]);
-            }
-            
-            //assign existing smart contract to user, log, and verifier
-            dataSubject = Agreement(userAddresses[1]);
-            logs = Log(logAddresses[1]);
-            v = Verification(verificationAddress[1]);
-
-            //set the consent information of users
-            for (uint i = 1; i < maxActor; i++){
-                if (i%2 ==1){
-                    dataSubject.setAgreement(actorAddresses[i], "test", false);
-                }
-                else{
-                    dataSubject.setAgreement(actorAddresses[i], "test", false);
-                }
-                
-            }
-
-            //pass actor, user, log to the verifier in order for autotesting
-            v.setInputs(actors, dataSubject, logs);
-
+        for (uint i = 1; i <= maxActor; i++){   //preset values to actors (DataUsage Contract)
+            actors.push(DataUsage(actorAddresses[i]));
+            actors[i-1].setActor(i, "test", "advertisement", "read", ["name", "address", "DOB", "job"]);
         }
+            
+        //assign existing smart contract to user, log, and verifier
+        dataSubject = Agreement(userAddresses[1]);
+        logs = Log(logAddresses[1]);
+        v = Verification(verificationAddress[1]);
+
+        //set the consent information of users
+        for (uint i = 1; i <= maxActor; i++){
+            if (i%2 ==1){
+                dataSubject.setAgreement(actorAddresses[i], "test", true);
+            }
+            else{
+                dataSubject.setAgreement(actorAddresses[i], "test", false);
+            }
+                
+        }
+
+        //pass actor, user, log to the verifier in order for autotesting
+        v.setInputs(actors, dataSubject, logs);
+
+        
     }
 
 
@@ -76,9 +74,9 @@ contract Simulator {
     function logAndValidation(uint maxActor) private{
         //for each actors, check if the user allows the purpose of data
         for (uint i = 1; i <= maxActor; i++){
-            bool consent = dataSubject.getAgreement(actorAddresses[actors[i-1].getDataUsageDetailByActorId(1)[0].actorId], actors[i-1].getDataUsageDetailByActorId(1)[0].purpose);
+            bool consent = dataSubject.getAgreement(actorAddresses[i], actors[i-1].getDataUsageDetailByActorId(i)[0].serviceName);
             if(consent) {
-                logs.setLog(i, actors[i-1].getDataUsageDetailByActorId(1)[0].operation, actors[i-1].getDataUsageDetailByActorId(1)[0].data, actors[i-1].getDataUsageDetailByActorId(1)[0].serviceName);
+                logs.setLog(i, actors[i-1].getDataUsageDetailByActorId(i)[0].operation, actors[i-1].getDataUsageDetailByActorId(i)[0].data, actors[i-1].getDataUsageDetailByActorId(i)[0].serviceName);
             }
         }
         //bool consent = dataSubject.getAgreement(0xd9145CCE52D386f254917e481eB44e9943F39138, "advertisement");
